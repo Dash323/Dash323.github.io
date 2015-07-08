eced59082b619b10ba2709fd5b3ab1b1eb755f3b
Dash323 <MatthewMorelandJames@gmail.com>
2015-07-08 11:27:14 -0500

    Added html img

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// Smooth Scrolling to different sections of the page.
+	// When "Services We Offer" button is clicked...
+		// smooth scroll to sevices we offer section
+
+	 // When "Accepted Insurance and Payment" button is clicked...
+		// smooth scroll to sevices we offer section
+
+	// When "Meet The Staff" button is clicked...
+		// smooth scroll to the sevices we offer section
+
+	
+// Hamburger menu for small screens
+	// When hamburger menu is clicked...
+		// increase spacing between hamburger lines
+		// toggle a class to nav bar that transitions from the nav having a height of 0 to a height of ~300px
+
+// Carousel of pictures and likes of the pictures
+	// When next button is clicked...
+		// if on second to last picture...
+			// disable next button
+		// un-disable previous button
+		// display next picture
+		// increase index number by 1
+
+	// When previous button is clicked...
+		// if on second picture...
+			// disable previous button
+		// un-disable next button
+		// display previous picture
+		// decrease index number by 1
+
+	// When like button is clicked...
+		// increase the number of likes for that picture
+
+	// When dislike button is clicked...
+		// decrease the number of likes for that picture
+
+// Side-menu toggle-slides from off the screen on the left side
+	// When hovering over the side menu icon
+		// Slide side menu onto screen from left side.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
