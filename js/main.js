326745cebc11e34d910432ebbe3b2c6bec49dc74
Dash323 <MatthewMorelandJames@gmail.com>
2015-07-20 03:53:25 -0500

    side bar button in good condition

--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@
 
 $('#vertlines').on('click', function(){
 	$('aside').toggleClass('open');
+	// $('header nav div span').animate({borderWidth: "3px 5px"});
 });
 
 $(window).on('scroll', function(){
@@ -53,7 +54,23 @@ $(window).on('scroll', function(){
 
 
 
+// Smooth scroll this mother!
 
+// Do it when someone clicks a nav link
+// $('nav a').on('click', function(e) {
+//   // prevent the standard link operation on click
+//   e.preventDefault();
+//   // use the href of the link to identify what
+//   // section to scroll to
+//   var thisTarget = $(this).attr('href');
+//   // get that section's top offset
+//   var targetOffset = $(thisTarget).offset().top;
+//   // use jQuery.animate() to animate the body's
+//   // scrollTop to the targetOffest
+//   $('body').animate({
+//     scrollTop: targetOffset
+//   }, 600);
+// });
 
 
 
